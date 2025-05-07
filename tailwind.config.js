ff3b91728081a11ec55f09a43d02278444d93919
Paul Kim <41440621+paul821@users.noreply.github.com>
2025-05-07 10:16:59 -0500

    Update fonts to use Newsreader and Rubik

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          sans: ['Rubik', 'sans-serif'],
+          serif: ['Newsreader', 'serif'],
+        },
+      },
     },
     plugins: [],
   }
